9a7c1596e4e47c1d3de4d5afbab60c46757230e9
ad1sjkee <ad1sjkeee@gmail.com>
2019-03-06 17:38:21 +0300

    update

--- src/app/base/main.js
+++ src/app/base/main.js
@@ -1,30 +1,47 @@
-$(function name() {
-    $("#button-1").click(function() {
-      $("#item-1").slideToggle("slow");
-    });
-    $("#button-2").click(function() {
-      $("#item-2").slideToggle("slow");
-    });
-    $("#button-3").click(function() {
-      $("#item-3").slideToggle("slow");
-    });
-    $("#button-4").click(function() {
-      $("#item-4").slideToggle("slow");
-    });
-})
-
-
 $(function windowSize() {
   if ($(window).width() <= '992') {
-    $(function name() {
-        
+    $(function collapse() {
+      $("#button-1").click(function() {
+        $("#item-1").slideToggle("slow");
+      });
+      $("#button-2").click(function() {
+        $("#item-2").slideToggle("slow");
+      });
+      $("#button-3").click(function() {
+        $("#item-3").slideToggle("slow");
+      });
+      $("#button-4").click(function() {
+        $("#item-4").slideToggle("slow");
+      });
     })
   } else {
-    $(function name() {
-      var flip = 0;
-      $("button").click(function() {
-        $("span").toggle(flip++ % 2 === 0);
-      });
+    $(function collapse() {
+      $(function collapse() {
+        $("#button-1").click(function() {
+          $("#item-1").toggle();
+          $("#item-2").hide();
+          $("#item-3").hide();
+          $("#item-4").hide();
+        });
+        $("#button-2").click(function() {
+          $("#item-2").toggle();
+          $("#item-1").hide();
+          $("#item-3").hide();
+          $("#item-4").hide();
+        });
+        $("#button-3").click(function() {
+          $("#item-3").toggle();
+          $("#item-1").hide();
+          $("#item-2").hide();
+          $("#item-4").hide();
+        });
+        $("#button-4").click(function() {
+          $("#item-4").toggle();
+          $("#item-1").hide();
+          $("#item-2").hide();
+          $("#item-3").hide();
+        });
+      })
     })
   }
 })
